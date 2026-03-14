9e88fc85399c0f62456cd5da246771c7a93c7cc0
jagter4 <mahmoud.t.alsayyad@gmail.com>
2026-03-14 14:55:21 +0200

    fix the function

--- src/extract.py
+++ src/extract.py
@@ -1,23 +1,28 @@
-import requests 
-import pandas  as pd
+import requests
+import pandas as pd
 from utils import send_request
 
-
-def extract_coin_list(as_frame = False): 
+def extract_coins_list(as_frame: bool = False) -> dict | pd.DataFrame:
+    """ Extracts the list of coins from CoinGecko API.
+    """
     endpoint = "/coins/list"
-    data =send_request(endpoint)
-    if as_frame: 
+    data = send_request(endpoint)
+    if as_frame:
         return pd.DataFrame(data)
-
     return data
 
-def extract_market_chart(coin_id, vs_currency = 'usd', days = 1): 
-    # function pulls crypto prices every 5 minute
-    endpoint = f'/coins/{coin_id}/market_chart'
+def extract_market_chart(coin_id: str, vs_currency: str = "usd", days: int = 1) -> dict:
+    """ Extracts the market chart data for a specific coin.
+    Args:
+        coin_id (str): The ID of the coin to extract data for.
+        vs_currency (str): The currency to convert to. Default is "usd".
+        days (int): The number of days to look back. Default is 1.
+    """
+    # The function that scrapes crypto prices in 5-minute increment
+    endpoint = f"/coins/{coin_id}/market_chart"
     params = {
-        'vs_currency': 'usd',
-        'days': days
+        "vs_currency": vs_currency,
+        "days": days,
     }
-
     data = send_request(endpoint, params=params)
-    return  data
+    return data
--- src/utils.py
+++ src/utils.py
@@ -1,18 +1,41 @@
 import requests
-from dotenv import load_dotenv
 from os import getenv
+from dotenv import load_dotenv
+from sqlalchemy import create_engine, Engine, URL
 
 load_dotenv()
-API_KEY= getenv("API_KEY")
+
+COINGECKO_API_KEY = getenv("COINGECKO_API_KEY")
 BASE_URL = "https://api.coingecko.com/api/v3"
 
-def send_request(endpoint, params=None): 
-    url =f'{BASE_URL}{endpoint}'
+def send_request(endpoint: str, params: dict = None) -> dict:
+    """ Sends a GET request to the CoinGecko API.
+    
+    Args:
+        endpoint (str): The API endpoint to send the request to.
+        params (dict, optional): The query parameters to include in the request.
+        
+    Returns:
+        dict: The JSON response from the API.
+    """
+    url = f"{BASE_URL}{endpoint}"
     headers = {
-        'accept': 'application/json',
-        'x-cg-demo-api-key':API_KEY
+        "accept": "application/json",
+        "x-cg-demo-api-key": COINGECKO_API_KEY,
     }
-
-    response = requests.get(url, headers=headers, params=params )
-    response.raise_for_status()
+    
+    response = requests.get(url, headers=headers, params=params)
+    response.raise_for_status()  # Raise an error for bad responses
     return response.json()
+
+def get_engine(db_user: str, db_password: str, db_host: str, db_port: str, db_name: str):
+    connection_string = URL.create(
+        "postgresql+psycopg2",
+        username=db_user,
+        password=db_password,
+        host=db_host,
+        port=db_port,
+        database=db_name
+    )
+
+    return create_engine(connection_string)
